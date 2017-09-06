2f3a1b6ea34d77f66481803c8d53cef3a255b556
Sabina <sabina.orunbaeva@gmail.com>
2017-09-06 18:56:23 +0600

    Add files via upload

---
+++ HomeworkExceptions/ConsoleApp2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (WebClient webClient = new WebClient())
+            {
+                string url = "https://www.youtabe.com";
+                try
+                {
+                    Console.WriteLine(webClient.OpenRead(url));
+                }
+                catch (System.Net.WebException exception)
+                {
+                    Console.WriteLine(exception.Message);
+                }
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ HomeworkExceptions/Task2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int index = 10;
+            Random rand = new Random();
+            int[] array = new int[index];
+            for (int i = 0; i < index; i++)
+            {
+                array[i] = rand.Next(100);
+            }
+            try
+            {
+                Console.WriteLine(array[index+10]);
+            }
+            catch (IndexOutOfRangeException ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine("Конец обработки массива");
+            }
+            
+            Console.ReadLine();
+        }
+    }
+}
